9dd95621ce591eca0db328e0570e62bd923bd29b
Andreas Wahlqvist <andreas@deepfriedmind.com>
2025-01-08 10:33:31 +0100

    chore: 🔧 update ESLint configuration and dependencies
    - Added `@nuxt/eslint` module to `nuxt.config.ts`.
    - Updated compatibility date to `2025-01-08`.
    - Introduced new `eslint.config.js` file with custom rules.
    - Added linting scripts to `package.json`.
    - Included new dependencies for ESLint and TypeScript.

---
+++ eslint.config.js
@@ -0,0 +1,149 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+import perfectionist from 'eslint-plugin-perfectionist'
+
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  antfu({
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
+    stylistic: false,
+    unicorn: {
+      allRecommended: true,
+    },
+    vue: true,
+  }),
+  {
+    name: 'project/global',
+    rules: {
+      ...perfectionist.configs['recommended-natural'].rules,
+      '@stylistic/operator-linebreak': ['error', 'before', { overrides: { '?': 'after' } }], // equivalent to Prettier's `experimentalTernaries` option
+      '@stylistic/padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', next: 'block-like', prev: 'expression' },
+        { blankLine: 'always', next: 'block-like', prev: 'function' },
+        { blankLine: 'always', next: 'block-like', prev: 'if' },
+        { blankLine: 'always', next: 'block-like', prev: 'switch' },
+        { blankLine: 'always', next: 'const', prev: 'block-like' },
+        { blankLine: 'always', next: 'export', prev: '*' },
+        { blankLine: 'any', next: 'export', prev: 'export' },
+        { blankLine: 'always', next: 'expression', prev: 'block-like' },
+        { blankLine: 'always', next: 'expression', prev: 'multiline-const' },
+        { blankLine: 'always', next: 'function', prev: 'block-like' },
+        { blankLine: 'always', next: 'function', prev: 'const' },
+        { blankLine: 'always', next: 'if', prev: 'block-like' },
+        { blankLine: 'always', next: 'if', prev: 'multiline-const' },
+        { blankLine: 'always', next: 'let', prev: 'block-like' },
+        { blankLine: 'always', next: 'multiline-const', prev: 'expression' },
+        { blankLine: 'always', next: 'multiline-const', prev: 'if' },
+        { blankLine: 'always', next: 'multiline-const', prev: 'multiline-const' },
+        { blankLine: 'always', next: 'return', prev: '*' },
+        { blankLine: 'always', next: 'switch', prev: '*' },
+      ],
+      '@stylistic/quote-props': ['error', 'as-needed'],
+      '@stylistic/quotes': ['error', 'single', { allowTemplateLiterals: false, avoidEscape: true }],
+      'arrow-body-style': 'error',
+      'import/order': [
+        'warn',
+        {
+          alphabetize: {
+            caseInsensitive: true,
+            order: 'asc',
+            orderImportKind: 'asc',
+          },
+          'newlines-between': 'always',
+        },
+      ],
+      'no-console': 'off',
+      'no-lonely-if': 'error',
+      'no-useless-concat': 'error',
+      'perfectionist/sort-imports': 'off',
+      'prefer-destructuring': [
+        'error',
+        {
+          AssignmentExpression: {
+            array: true,
+            object: false,
+          },
+          VariableDeclarator: {
+            array: false,
+            object: true,
+          },
+        },
+        {
+          enforceForRenamedProperties: false,
+        },
+      ],
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+          ignore: [String.raw`^README\.md$`],
+        },
+      ],
+      'unicorn/prevent-abbreviations': ['error', { checkFilenames: false }],
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    name: 'project/vue',
+    rules: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: { pascalCase: true },
+          ignore: ['^app\.vue$'],
+        },
+      ],
+      'vue/camelcase': 'error',
+      'vue/first-attribute-linebreak': [
+        'error',
+        {
+          singleline: 'beside',
+        },
+      ],
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          multiline: 1,
+          singleline: 1,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.md'],
+    name: 'project/markdown',
+    rules: {
+      '@stylistic/no-trailing-spaces': 'off',
+    },
+  },
+  {
+    files: ['package.json'],
+    name: 'project/packagejson',
+    rules: { 'jsonc/sort-keys': 'off' },
+  },
+  {
+    files: ['utils/**/*', 'composables/**/*'],
+    name: 'project/misc',
+    rules: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'camelCase',
+        },
+      ],
+    },
+  },
+  {
+    files: ['nuxt.config.ts'],
+    name: 'project/nuxtconfig',
+    rules: {
+      'perfectionist/sort-objects': 'off',
+    },
+  },
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  modules: ['@nuxt/eslint'],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-01-08',
+  eslint: {
+    config: {
+      standalone: false,
+      stylistic: true,
+    },
+  },
 })
