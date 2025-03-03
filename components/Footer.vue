0ccf95a1002ae6691f20ecfe8e811b89c0630f36
Andreas Wahlqvist <andreas@deepfriedmind.com>
2025-03-03 20:45:12 +0100

    feat(footer): ✨ enhance footer layout for improved responsiveness

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,17 +9,22 @@ const dayjs = useDayjs()
     class="bg-[linear-gradient(0,_rgb(23_41_64_/_33%),_transparent)] pb-8 pt-2.5 text-center"
   >
     <div class="container [text-shadow:1px_1px_0px_rgb(36_65_102_/_50%)]">
-      <p class="font-medium">
-        &copy;{{ dayjs(buildTime).year() }}
-        <Logo class="text-lg" />– Made with <img
-          src="/img/fc.webp"
-          width="24"
-          height="24"
-          alt="Fire Crystal"
-          class="inline align-top"
-        > by [BAM]WhaleQuest from #1459 – Last updated: {{ dayjs(buildTime).format('YYYY-MM-DD') }}
+      <p class="space-y-2 font-medium">
+        <span class="max-lg:block">&copy;{{ dayjs(buildTime).year() }}
+          <Logo class="text-xl" />
+        </span>
+        <span class="max-lg:hidden">– </span><span class="max-lg:block">
+          Made with <img
+            src="/img/fc.webp"
+            width="24"
+            height="24"
+            alt="Fire Crystal"
+            class="inline align-top"
+          > by [BAM]WhaleQuest from #1459
+        </span>
+        <span class="max-lg:hidden">– </span><span class="max-lg:block">Last updated: {{ dayjs(buildTime).format('YYYY-MM-DD') }}</span>
       </p>
-      <p class="text-xs">
+      <p class="mt-2 text-xs">
         This website has no affiliation with Century Games Pte. Ltd.
       </p>
     </div>
