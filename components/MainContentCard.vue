cc0a4c5a879444025fba50b1e1e90a139aa11cea
Andreas Wahlqvist <andreas@deepfriedmind.com>
2025-04-03 23:16:34 +0200

    refactor: ♻️ make main background a `<picture>` element with art direction

--- components/MainContentCard.vue
+++ components/MainContentCard.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="animate-fadeinright rounded-3xl bg-surface-950/95 p-4 shadow-2xl shadow-blue-200 backdrop-blur-md animate-once sm:p-8">
+  <div class="animate-fadeinright rounded-3xl bg-surface-950/95 p-4 shadow-2xl shadow-blue-950 backdrop-blur-md animate-once sm:p-8">
     <div
       v-if="heading || subHeading"
       class="mb-12 w-11/12 space-y-4"
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="grid min-h-screen grid-rows-[auto_1fr_auto] gap-8">
+  <div class="relative grid min-h-screen grid-rows-[auto_1fr_auto] gap-8">
+    <div class="absolute inset-0 -z-50">
+      <picture>
+        <source
+          type="image/avif"
+          media="(min-width: 3072px)"
+          srcset="/img/bg-h-3072.avif"
+        >
+        <source
+          type="image/avif"
+          media="(min-width: 2560px)"
+          srcset="/img/bg-h-2560.avif, /img/bg-h-3072.avif 2x"
+        >
+        <source
+          type="image/avif"
+          media="(min-width: 1536px"
+          srcset="/img/bg-h-1536.avif, /img/bg-h-3072.avif 2x"
+        >
+        <source
+          type="image/avif"
+          media="(min-width: 1024px)"
+          srcset="/img/bg-h-1280.avif, /img/bg-h-2560.avif 2x"
+        >
+        <source
+          type="image/avif"
+          media="(min-width: 640px)"
+          srcset="/img/bg-v-768.avif, /img/bg-v-1280.avif 2x"
+        >
+        <img
+          fetchpriority="high"
+          src="/img/bg-v-384.avif"
+          srcset="/img/bg-v-768.avif 2x"
+          alt="A background image of a snowy mountain landscape"
+          class="size-full object-contain object-bottom"
+        >
+      </picture>
+    </div>
     <Header />
     <main class="p-4 py-container 2xl:container sm:p-8 xl:p-16">
       <NuxtRouteAnnouncer />
