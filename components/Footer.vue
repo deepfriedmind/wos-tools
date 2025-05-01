3c3f595c96c8c31a6e2caf9379a31f0bdd549181
Andreas Wahlqvist <andreas@deepfriedmind.com>
2025-05-02 01:08:55 +0200

    refactor(footer): ♻️ update date formatting and improve build time handling
    Refactored the footer component to utilize `$dayjs` for date formatting
    and adjusted padding for better visual consistency.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 const { public: { buildTime } } = useRuntimeConfig()
-const dayjs = useDayjs()
 </script>
 
 <template>
   <footer
     v-if="typeof buildTime === 'string'"
-    class="bg-[linear-gradient(0,_rgb(23_41_64_/_33%),_transparent)] pb-8 pt-2.5 text-center"
+    class="bg-[linear-gradient(0,_rgb(23_41_64_/_33%),_transparent)] pb-24 pt-2.5 text-center"
   >
     <div class="container [text-shadow:1px_1px_0px_rgb(36_65_102_/_50%)]">
       <p class="space-y-2 font-medium">
-        <span class="max-lg:block">&copy;{{ dayjs(buildTime).year() }}
+        <span class="max-lg:block">&copy;{{ $dayjs(buildTime).year() }}
           <RouterLink
             to="/"
             aria-label="Home"
@@ -27,7 +26,7 @@ const dayjs = useDayjs()
             class="inline align-top"
           > by [BAM]WhaleQuest from #1459
         </span>
-        <span class="max-lg:hidden">– </span><span class="max-lg:block">Last updated: {{ dayjs(buildTime).format('YYYY-MM-DD') }}</span>
+        <span class="max-lg:hidden">– </span><span class="max-lg:block">Last updated: <time :datetime="buildTime">{{ $dayjs(buildTime).format('YYYY-MM-DD') }}</time></span>
       </p>
       <p class="mt-2 text-xs">
         This website has no affiliation with Century Games Pte. Ltd.
