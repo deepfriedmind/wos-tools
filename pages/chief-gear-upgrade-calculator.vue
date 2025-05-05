fd6fa36bbebdf3429d84ffc32d1c48ee7fb651e7
Andreas Wahlqvist <andreas@deepfriedmind.com>
2025-05-05 06:34:36 +0200

    refactor(chief-gear): ♻️ update icon color classes for gear pieces

--- pages/chief-gear-upgrade-calculator.vue
+++ pages/chief-gear-upgrade-calculator.vue
@@ -2,7 +2,7 @@
 const PAGE_TITLE = 'Chief Gear Upgrade Calculator'
 const PAGE_DESCRIPTION = 'Calculate the material costs for upgrading Chief Gear'
 const PAGE_ICON = 'game-icons:pirate-coat'
-const PAGE_ICON_COLOR_CLASS = 'bg-gradient-to-tr from-yellow-900 to-yellow-900 via-yellow-700'
+const PAGE_ICON_COLOR_CLASS = 'bg-gradient-to-r from-yellow-700 to-yellow-700 via-yellow-900'
 
 definePageMeta({
   description: `${PAGE_DESCRIPTION} in Whiteout Survival.`,
