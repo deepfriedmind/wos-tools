83e2d4b424aa5b289a04765809f0b714f6de3a70
Andreas Wahlqvist <andreas@deepfriedmind.com>
2025-06-30 15:46:41 +0200

    refactor(Footer): ♻️ simplify footer layout and remove unneeded elements

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,23 +2,13 @@
   <footer class="bg-[linear-gradient(0,_rgb(23_41_64_/_33%),_transparent)] pb-24 pt-2.5 text-center">
     <div class="container [text-shadow:1px_1px_0px_rgb(36_65_102_/_50%)]">
       <p class="space-y-2 font-medium">
-        <span class="max-lg:block">&copy;{{ $dayjs().year() }}
-          <RouterLink
-            to="/"
-            aria-label="Home"
-          >
-            <Logo class="text-xl" />
-          </RouterLink>
-        </span>
-        <span class="max-lg:hidden">– </span><span class="max-lg:block">
-          Made with <img
-            src="/img/fc.webp"
-            width="24"
-            height="24"
-            alt="Fire Crystal"
-            class="inline align-top"
-          > by [BAM]WhaleQuest from #1459
-        </span>
+        &copy;{{ $dayjs().year() }}
+        <RouterLink
+          to="/"
+          aria-label="Home"
+        >
+          <Logo class="text-xl" />
+        </RouterLink>
       </p>
       <p class="mt-2 text-xs">
         This website has no affiliation with Century Games Pte. Ltd.
